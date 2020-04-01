d34d9ba90681bd94f2113533b784e715d56708b7
ethanmjansen <ethanm.jansen@gmail.com>
2020-04-01 11:45:58 -0600

    Added functionality and requirments.txt and procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dnd_api:app -t 120
--- dnd_api/app.py
+++ dnd_api/app.py
@@ -1,25 +1,51 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
+from MonsterGen import Monster, CR, random_trap, Npc, monster_loot, horde_loot
 
 def create_app():
+
     app = Flask(__name__)
 
-    @app.route('/monsters')
-    def hello_world_monster():
-      return 'Hello, monsters!'
+    @app.route('/monster')
+    def monster():
+        level = int(request.args['avg-level'])
+        players = int(request.args['num-players'])
+        difficulty = int(request.args['difficulty'])
+        return Monster(CR.party_adapter(
+            average_level=level,
+            num_players=players,
+            difficulty=difficulty,
+        )).to_dict()
+
+    @app.route('/monster-cr-type')
+    def monster_cr():
+        return Monster(
+            int(request.args['cr']),
+            request.args['type'],
+        ).to_dict()
 
     @app.route('/treasure')
-    def hello_world_treasure():
-      return 'Hello, treasure!'
+    def treasure():
+        return monster_loot(int(request.args['cr'])).to_dict()
+
+    @app.route('/treasure-horde')
+    def treasure_horde():
+        return horde_loot(int(request.args['cr'])).to_dict()
 
     @app.route('/trap')
-    def hello_world_trap():
-      return 'Hello, trap!'
+    def trap():
+        return random_trap(int(request.args['cr'])).to_dict()
+
+    @app.route('/trap-type')
+    def trap_type():
+        cr = int(request.args['cr'])
+        damage_type = request.args['type']
+        return random_trap(cr, damage_type).to_dict()
 
     @app.route('/npc')
-    def hello_world_npc():
-      return 'Hello, NPC!'
+    def npc():
+        return Npc().to_dict()
 
     return app
-
+    
 if __name__ == "__main__":
-    app = create_app()
+    create_app().run()

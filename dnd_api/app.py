345d69aa6631da432e23543f38cc6f6d895745df
Robert Sharp <webmaster@sharpdesigndigital.com>
2020-04-09 11:03:21 -0700

    pep8

--- dnd_api/app.py
+++ dnd_api/app.py
@@ -3,6 +3,7 @@ from MonsterGen import Monster, CR, random_trap, Npc, monster_loot, horde_loot
 
 app = Flask(__name__)
 
+
 @app.route('/monster')
 def monster():
     level = int(request.args['avg-level'])
@@ -14,6 +15,7 @@ def monster():
         difficulty=difficulty,
     )).to_dict()
 
+
 @app.route('/monster-cr-type')
 def monster_cr():
     return Monster(
@@ -21,23 +23,28 @@ def monster_cr():
         request.args['type'],
     ).to_dict()
 
+
 @app.route('/treasure')
 def treasure():
     return monster_loot(int(request.args['cr'])).to_dict()
+
 
 @app.route('/treasure-horde')
 def treasure_horde():
     return horde_loot(int(request.args['cr'])).to_dict()
 
+
 @app.route('/trap')
 def trap():
     return random_trap(int(request.args['cr'])).to_dict()
+
 
 @app.route('/trap-type')
 def trap_type():
     cr = int(request.args['cr'])
     damage_type = request.args['type']
     return random_trap(cr, damage_type).to_dict()
+
 
 @app.route('/npc')
 def npc():
@@ -46,21 +53,22 @@ def npc():
 
 @app.before_request
 def before_request():  # CORS preflight
-	def _build_cors_prelight_response():
-		response = make_response()
-		response.headers.add("Access-Control-Allow-Origin", "*")
-		response.headers.add("Access-Control-Allow-Headers", "*")
-		response.headers.add("Access-Control-Allow-Methods", "*")
-		return response
-	if request.method == "OPTIONS":
-		return _build_cors_prelight_response()
+    def _build_cors_prelight_response():
+        response = make_response()
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Headers", "*")
+        response.headers.add("Access-Control-Allow-Methods", "*")
+        return response
+    if request.method == "OPTIONS":
+        return _build_cors_prelight_response()
 
 
 @app.after_request
 def after_request(response):  # CORS headers
-	header = response.headers
-	header['Access-Control-Allow-Origin'] = '*'
-	return response
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    return response
+
 
 if __name__ == "__main__":
     app.run()
